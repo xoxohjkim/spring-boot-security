acfdc7fc68dc10ed56a2b22a1fa7e48ae3143a6f
hjkim <xoxohjkim@gmail.com>
2022-01-07 00:04:58 +0900

    session management

--- src/main/java/io/security/basicsecurity/SecurityConfig.java
+++ src/main/java/io/security/basicsecurity/SecurityConfig.java
@@ -12,6 +12,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -32,7 +33,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		
 		// * handler에서 로그인,로그아웃 시 구현하고 싶은 로직 구현
 		
-		// 인가  - 어떤 요청이라도 인증을 받겠다는 정책
+		// * 인가  - 어떤 요청이라도 인증을 받겠다는 정책
 		//	       루트경로로 접근 시 로그인
 		http
 			.authorizeRequests() 
@@ -64,7 +65,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 				.userDetailsService(userDetailService);
 		
 		
-		// 인증 - form login으로 인증
+		// * 인증 - form login으로 인증
 		http
 			.formLogin()
 				
@@ -98,7 +99,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 												//로그인 페이지 permitAll
 		
 		
-		// 로그아웃 - 기본적으로 post 이나 get도 가능
+		// * 로그아웃 - 기본적으로 post 이나 get도 가능
 		http
 			.logout()
 				.logoutUrl("/logout")
@@ -122,7 +123,38 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 					}
 				})
 				.deleteCookies("remember-me");		//로그인 시 remember-me이름으로 쿠키발급됨 - 삭제하기
+		
+		
+		// * 동시 세션 제어 
+//		http
+//			.sessionManagement()
+//				.maximumSessions(1) 					// 최대 허용가능 세션 수, -1이면 무제한
+//				.maxSessionsPreventsLogin(false)		// 동시 로그인 차단함. true일 시 로그인도 불가능. false일 시 먼저 로그인한 계정 session 만료뜸. default: false(기존 세션 만료)
+//				.invalidSessionUrl("/invalid")		// 세션이 유효하지 않을 때 이동할 페이지
+//				.expiredUrl("/expired");				// 세션이 만료된 경우 이동할 페이지
+				
+		
+		// * 세션고정보호 - 공격자 세션쿠키로 로그인 시도하더라도 로그인 인증시 새로운 세션id 발급하여 보호
+		http
+			.sessionManagement()
+			.sessionFixation()
+			.changeSessionId();	// 기본값(servlet3.1 이상) - 새로운 세션 id 								
+			// none - 보호 x
+			// migrateSession - 기본값(servlet3.1 이하) - 새로운 세션 id. 속성값 유지
+			// newSession - 새로운 세션, 새로운 세션 id. 속성값 유지 X
+		
+		// * 세션 정책
+		http
+			.sessionManagement()
+			.sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED);		// 스프링시큐리티가 필요 시 생성(기본값)
+			
+			//SessionCreationPolicy.ALWAYS		: 스프링시큐리티가 항상 세션 생성
+			//SessionCreationPolicy.IF_REQUERED	: 스프링시큐리티가 필요 시 세션 생성(기본값)
+			//SessionCreationPolicy.NEVER		: 스프링 시큐리티가 생성하지 않지만 이미 존재하면 사용
+			//SessionCreationPolicy.STATELESS	: 스프링 시큐리티가 생성하지 않고 존재해도 사용하지 않음
+				// => stateless 사용 : session cookie가 아니고 session을 사용하지 않을 때.
+				// 	  ex) json web token(JWT)사용 시. 토큰에 사용자정보 등 저장하고 세션없이 인증받는 방식
+		
 		}
-
 
 }
